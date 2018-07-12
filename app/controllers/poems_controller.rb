153b58b874d6c5f00d1aae7fd6b23760ddbd6179
americanmouths <shiverr@gmail.com>
2018-07-11 19:16:23 -0700

    create new poem form, submitting and displaying correctly

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,2 +1,20 @@
 class PoemsController < ApplicationController
+
+  def new
+    @poem = Poem.new
+  end
+
+  def create
+    @poem = Poem.create(poem_params)
+    redirect_to poem_path(@poem)
+  end
+
+  def show
+    @poem = Poem.find_by(id: params[:id])
+  end
+
+  private
+  def poem_params
+    params.require(:poem).permit(:title, :content, :category_name, :user_id)
+  end
 end
--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,2 +1,12 @@
 class Poem < ApplicationRecord
+  belongs_to :category
+
+  def category_name=(name)
+     self.category = Category.find_or_create_by(name: name)
+   end
+
+   def category_name
+      self.category ? self.category.name : nil
+   end
+   
 end
