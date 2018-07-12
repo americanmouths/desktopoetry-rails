988cecbeea4fddc45dda8504cbc83d978e6e10c2
americanmouths <shiverr@gmail.com>
2018-07-11 19:22:48 -0700

    add date to poem form, update params

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -11,10 +11,11 @@ class PoemsController < ApplicationController
 
   def show
     @poem = Poem.find_by(id: params[:id])
+    @category = Category.find_by(name: @poem.category_name)
   end
 
   private
   def poem_params
-    params.require(:poem).permit(:title, :content, :category_name, :user_id)
+    params.require(:poem).permit(:title, :content, :category_name, :user_id, :date)
   end
 end
