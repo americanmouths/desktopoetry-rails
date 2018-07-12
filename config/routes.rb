392623abdfb414f73c4b30f930c63da43c7d6e95
americanmouths <shiverr@gmail.com>
2018-07-11 18:33:13 -0700

    add name to sign up, display name in navbar on sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :poems
 
   root 'home#index'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
