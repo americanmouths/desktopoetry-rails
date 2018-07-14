91d4c80301014a502c02205f4a41d1338cec7760
americanmouths <shiverr@gmail.com>
2018-07-14 14:42:25 -0700

    start building out category views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,9 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = current_user.categories
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+  end
 end
