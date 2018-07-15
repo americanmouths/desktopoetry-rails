6536c75191d44ea08a9fb15639a8b6a62c05f85f
americanmouths <shiverr@gmail.com>
2018-07-15 12:20:11 -0700

    create poem_ids method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,14 @@
 class Category < ApplicationRecord
   has_many :poems
   belongs_to :user
+  validates :name, :presence => true
+
+  def poem_ids=(ids)
+    ids.each do |id|
+      poem = Poem.find_by(id: id)
+      if !self.poems.include?(poem)
+        self.poems << poem
+      end
+    end
+  end
 end
