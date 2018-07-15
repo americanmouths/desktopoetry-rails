0897ab0e812805fe2c09de63f6942585df5fa9fe
americanmouths <shiverr@gmail.com>
2018-07-15 12:21:12 -0700

    add validations to poem

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,6 +1,9 @@
 class Poem < ApplicationRecord
   belongs_to :category
   belongs_to :user
+  validates :title, :presence => true
+  validates :date, :presence => true
+  validates :content, :presence => true
 
   def category_name=(name)
      self.category = Category.find_or_create_by(name: name)
